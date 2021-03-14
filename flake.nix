16fdf6cdcdfc846f86130d6f1eb4a421372ca52a
Mason Mackaman <masondeanm@aol.com>
2021-03-13 19:29:53 -0500

    Flakify

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{ outputs = { nixpkgs, utils, ...}:
+    utils.defaultSystems
+      ({ pkgs, ... }:
+        { packages =
+            { cursors =
+                { breeze = with pkgs;
+                    stdenv.mkDerivation
+                      { name = "Breeze";
+                        src = ./.;
+                        installPhase = "mv cursors/Breeze/Breeze $out";
+                      };
+                };
+            };
+        }
+      )
+      nixpkgs;
+}

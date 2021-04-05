4bf6856a977ab9d8a9c890dd479ce2548c73c924
Mason Mackaman <masondeanm@aol.com>
2021-04-05 13:40:12 -0400

    Formatting

--- flake.nix
+++ flake.nix
@@ -1,17 +1,17 @@
 { outputs = { nixpkgs, utils, ...}:
     utils.defaultSystems
       ({ pkgs, ... }:
-        { packages =
-            { cursors =
-                { breeze = with pkgs;
-                    stdenv.mkDerivation
-                      { name = "Breeze";
-                        src = ./.;
-                        installPhase = "mv cursors/Breeze/Breeze $out";
-                      };
-                };
-            };
-        }
+         { packages =
+             { cursors =
+                 { breeze = with pkgs;
+                     stdenv.mkDerivation
+                       { name = "Breeze";
+                         src = ./.;
+                         installPhase = "mv cursors/Breeze/Breeze $out";
+                       };
+                 };
+             };
+         }
       )
       nixpkgs;
 }
